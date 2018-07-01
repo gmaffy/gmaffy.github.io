af10c41a11b7be72dcac4a58362c61a0b20f2ed9
gmaffy <mafireyi@gmail.com>
2018-07-01 14:24:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ self.addEventListener('install', function (event) {
             return cache.addAll([
                 '/',
                 'js/main.js',
+                'js/idb.js'
                 'css/style.css',
             
             ]);
